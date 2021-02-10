c6f2654e9d64312f0033bfd1dbb14694fbc09cf4
Sandra To <sandra.to.e@gmail.com>
2021-02-10 06:17:37 +0000

    Updated files with compass codes and notes

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,21 +1,20 @@
 // asyncBreeds.js
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
-  console.log('this gets read first');
+const breedDetailsFromFile = function(breed, callbackFuncWhenThingsAreDone) {
   console.log('breedDetailsFromFile: Calling readFile...');
   
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log('this gets read last');
+    // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
-    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    if (!error) callbackFuncWhenThingsAreDone(data);
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay');
-console.log('this gets read second');
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+// CHANGE 1: Moved the console.log into a new function
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed); // => after change, will print out correctly when called
+};
+
+// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
+breedDetailsFromFile('Bombay', printOutCatBreed);
